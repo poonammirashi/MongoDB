ebd05bee842cb27eda8f3f4ab174ca94c482ea54
poonammirashi <poonam200023@gmail.com>
2023-09-03 03:42:31 -0700

    starting mongoose

--- app.js
+++ app.js
@@ -4,9 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
-const mongoConnect = require('./util/database').mongoConnect;
-const User = require('./models/user');
-
+const mongoose = require('mongoose')
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -18,20 +16,23 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use((req, res, next) => {
-  User.findById('5baa2528563f16379fc8a610')
-    .then(user => {
-      req.user = new User(user.name, user.email, user.cart, user._id);
-      next();
-    })
-    .catch(err => console.log(err));
-});
+// app.use((req, res, next) => {
+//   User.findById('5baa2528563f16379fc8a610')
+//     .then(user => {
+//       req.user = new User(user.name, user.email, user.cart, user._id);
+//       next();
+//     })
+//     .catch(err => console.log(err));
+// });
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoConnect(() => {
-  app.listen(3000);
-});
+mongoose.connect('mongodb+srv://poonam:gautam23@cluster0.oeyakd6.mongodb.net/')
+.then(() => {
+  console.log('connected');
+  app.listen(3000, () => console.log("server is running on port 3000"))
+})
+.catch(err => console.log(err))
